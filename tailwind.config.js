0ff578fd6f029da5ce49c59f153d578f41b2cddf
ThomasBarnekow <thomas@barnekow.info>
2024-08-20 22:05:07 +0200

    Fix tailwind.config.js and add build target

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./Layout/**/*.{razor,html,cshtml,css}",
-    "./Pages/**/*.{razor,html,cshtml,css}",
-    "./node_modules/flowbite/dist/flowbite.min.{js,css}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+    content: [
+        "./wwwroot/index.html",
+        "./Layout/**/*.{razor,html,cshtml}",
+        "./Pages/**/*.{razor,html,cshtml}",
+        "./node_modules/flowbite/dist/flowbite.min.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
